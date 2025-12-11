bf9242a5fa2095c0a625d6bd35f21bb699f996df
Oliva Galvez Garcia <U172169@eurocajarural.es>
2025-12-11 12:00:51 +0100

    servicio inmueble

---
+++ Models/Backend/Inmueble/CategoryResponse.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace ShopApp.Models.Backend.Inmueble;
+
+public class CategoryResponse
+{
+    public int Id { get; set; }
+    [JsonProperty("nombre")]
+    public string? NombreCategory { get; set; }
+    public string? ImagenUrl { get; set; }
+}
