d6d38acbddf392d20e4c6fbdb5a8fc79b82c529a
Oliva Galvez Garcia <U172169@eurocajarural.es>
2025-12-11 14:39:44 +0100

    Interfaz registro sin terminar

--- ViewModels/RegistroViewModel.cs
+++ ViewModels/RegistroViewModel.cs
@@ -1,9 +1,49 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ShopApp.ViewModels;
 
 public partial class RegistroViewModel : ViewModelGlobal
 {
+    [ObservableProperty]
+    private string nombre;
+
+    [ObservableProperty]
+    private string apellido;
+
+    [ObservableProperty]
+    private string email;
+
+    [ObservableProperty]
+    private string username;
+
+    [ObservableProperty]
+    private string telefono;
+
+    [ObservableProperty]
+    private string password;
+
+    [RelayCommand]
+    private async Task RegistrarUsuario()
+    {
+        if (string.IsNullOrWhiteSpace(Nombre) ||
+            string.IsNullOrWhiteSpace(Apellido) ||
+            string.IsNullOrWhiteSpace(Email) ||
+            string.IsNullOrWhiteSpace(Username) ||
+            string.IsNullOrWhiteSpace(Telefono) ||
+            string.IsNullOrWhiteSpace(Password))
+        {
+            await Application.Current.MainPage.DisplayAlert("Error", "Todos los campos son obligatorios.", "Aceptar");
+            return;
+        }
+
+        // Lógica para registrar al usuario...
+
+        await Application.Current.MainPage.DisplayAlert("Éxito", "Usuario registrado correctamente.", "Aceptar");
+        await Shell.Current.GoToAsync("..");
+    }
+
     [RelayCommand]
     private async Task NavegarLogin()
     {
