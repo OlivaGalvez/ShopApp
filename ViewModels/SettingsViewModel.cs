6dc61e46257bff393b15049741207fa47d9caf57
Oliva Galvez Garcia <U172169@eurocajarural.es>
2025-12-12 12:47:02 +0100

    Pagina configuracion terminada

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,5 +1,23 @@
-﻿namespace ShopApp.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using ShopApp.Services;
+using ShopApp.Views;
+
+namespace ShopApp.ViewModels;
 
 public partial class SettingsViewModel : ViewModelGlobal
 {
+    private INavegacionService _navegacionService;
+
+    [RelayCommand]
+    async Task SalirSesion()
+    {
+        Preferences.Set("accesstoken", string.Empty);
+        var uri = $"//{nameof(AboutPage)}";
+        await _navegacionService.GoToAsync(uri);
+    }
+
+    public SettingsViewModel(INavegacionService navegacionService)
+    {
+        _navegacionService = navegacionService;
+    }
 }
