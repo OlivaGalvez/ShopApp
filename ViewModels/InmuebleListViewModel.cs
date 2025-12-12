e1c9f6e1a7c5f604c6606792ae65142f4d3ecf99
Oliva Galvez Garcia <U172169@eurocajarural.es>
2025-12-12 10:02:38 +0100

    Detalle de Página con Diseño Avanzado

--- ViewModels/InmuebleListViewModel.cs
+++ ViewModels/InmuebleListViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using ShopApp.Models.Backend.Inmueble;
 using ShopApp.Services;
+using ShopApp.Views;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace ShopApp.ViewModels;
 
 public partial class InmuebleListViewModel : ViewModelGlobal, IQueryAttributable
 {
-    private readonly INavegacionService _navegacionService;
+    [ObservableProperty]
+    InmuebleResponse inmuebleSeleccionado;
 
     [ObservableProperty]
     ObservableCollection<InmuebleResponse> inmuebles;
+
+    private readonly INavegacionService _navegacionService;
 
     private readonly InmuebleService _inmuebleService;
 
@@ -19,6 +24,16 @@ public partial class InmuebleListViewModel : ViewModelGlobal, IQueryAttributable
     {
         _navegacionService = navegacionService;
         _inmuebleService = inmuebleService;
+        PropertyChanged += OnPropertyChanged;
+    }
+
+    private async void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(InmuebleSeleccionado))
+        {
+            var uri = $"{nameof(InmuebleDetailPage)}?id={InmuebleSeleccionado.Id}";
+            await _navegacionService.GoToAsync(uri);
+        }
     }
 
     public async void ApplyQueryAttributes(IDictionary<string, object> query)
