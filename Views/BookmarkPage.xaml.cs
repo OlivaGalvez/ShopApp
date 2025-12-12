43cd2c4fd93bfaa81c1d92387040a35c3351376a
Oliva Galvez Garcia <U172169@eurocajarural.es>
2025-12-12 11:27:45 +0100

    Fix

--- Views/BookmarkPage.xaml.cs
+++ Views/BookmarkPage.xaml.cs
@@ -1,12 +1,21 @@
 using ShopApp.ViewModels;
+using System.Threading.Tasks;
 
 namespace ShopApp.Views;
 
 public partial class BookmarkPage : ContentPage
 {
+    private BookmarkViewModel _viewModel;
+
 	public BookmarkPage(BookmarkViewModel viewModel)
 	{
 		InitializeComponent();
 		BindingContext = viewModel;
+        _viewModel = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        _viewModel.GetInmueblesCommand.Execute(this);
     }
 }
