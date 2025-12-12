93d0c0db7a0c265b44d1233b6aed8fc2c2dfacd3
Oliva Galvez Garcia <U172169@eurocajarural.es>
2025-12-12 10:29:37 +0100

    Eventos con Backend sin terminar

--- ViewModels/InmuebleDetailViewModel.cs
+++ ViewModels/InmuebleDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using ShopApp.Models.Backend.Inmueble;
 using ShopApp.Services;
 
@@ -43,5 +44,104 @@ public partial class InmuebleDetailViewModel : ViewModelGlobal, IQueryAttributab
     {
         var id = int.Parse(query["id"].ToString());
         await LoadDataAsync(id);
+    }
+
+    [RelayCommand]
+    async Task GetBackEvent()
+    { 
+        await _navegacionService.GoToAsync("..");
+    }
+
+    [RelayCommand]
+    async Task SaveBookmark()
+    {
+        var bookmark = new BookmarkRequest
+        {
+            InmuebleId = Inmueble.Id,
+            UsuarioId = Preferences.Get("userId", string.Empty)
+        };
+
+        await _inmuebleService.SaveBookmark(bookmark);
+        await LoadDataAsync(Inmueble.Id);
+    }
+
+    [RelayCommand]
+    async Task CallOwner()
+    {
+        var confirmarLlamada = Application.Current.MainPage.DisplayAlert(
+            "Marca este numero telefonico",
+            $"Desea llamar a este numero: {Inmueble.Telefono}",
+            "Si",
+            "No");
+
+        if (await confirmarLlamada)
+        {
+            try
+            {
+                PhoneDialer.Open(Inmueble.Telefono);
+            }
+            catch (ArgumentNullException)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "No se puede realizar esta llamada", 
+                    "El numero telefonico no es valido",
+                    "OK");
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "No se puede realizar esta llamada",
+                    "El dispositivo no soporta llamadas telefonicas",
+                    "OK");
+            }
+            catch (Exception)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "No se puede realizar esta llamada",
+                    "Errores en la marcacion del numero",
+                    "OK");
+            }
+        }
+    }
+
+    [RelayCommand]
+    async Task TextMessageOwner()
+    {
+        var message = new SmsMessage("Hola, por favor enviame info sobre la vivienda", Inmueble.Telefono);
+
+        var confirmarMensajeTexto = Application.Current.MainPage.DisplayAlert(
+            "Envia un mensaje de texto",
+            $"Desea enviar un mensaje de texto a este numero: {Inmueble.Telefono}",
+            "Si",
+            "No");
+
+        if (await confirmarMensajeTexto)
+        {
+            try
+            {
+                await Sms.ComposeAsync(message);
+            }
+            catch (ArgumentNullException)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "No se puede enviar este sms",
+                    "El numero telefonico no es valido",
+                    "OK");
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "No se puede enviar este sms",
+                    "El dispositivo no soporta envio de sms",
+                    "OK");
+            }
+            catch (Exception)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "No se puede enviar este sms",
+                    "Errores en el envio del sms",
+                    "OK");
+            }
+        }
     }
 }
